a32dc232828e27323bc1430e774c84cf861f78fb
SayfAhmed <98549166+SayfAhmed@users.noreply.github.com>
2023-04-06 11:22:49 +0100

    yesss

--- js/music.js
+++ js/music.js
@@ -1,9 +1,9 @@
-var audio=new Audio('https://vgmsite.com/soundtracks/kingdom-hearts-hd-1.5-2.5-remix-original-soundtrack-box-2014/eetmhzjfqa/7-11%20Darkness%20of%20the%20Unknown.mp3')
+let audio=new Audio('https://vgmsite.com/soundtracks/kingdom-hearts-hd-1.5-2.5-remix-original-soundtrack-box-2014/eetmhzjfqa/7-11%20Darkness%20of%20the%20Unknown.mp3')
 audio.loop=true;
 audio.play()
 
 function changeMusic(){
-    audio = new Audio('https://vgmsite.com/soundtracks/kingdom-hearts-hd-2.5-remix/sgwlmegpgq/4-10%20A%20Fight%20to%20the%20Death.mp3')
+    let audio= new Audio('https://vgmsite.com/soundtracks/kingdom-hearts-hd-2.5-remix/sgwlmegpgq/4-10%20A%20Fight%20to%20the%20Death.mp3')
     audio.loop=true;
     audio.play()
 }
